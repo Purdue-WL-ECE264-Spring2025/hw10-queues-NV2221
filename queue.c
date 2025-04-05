94d79b81f4b2f5949517eb59a4f532031baaf1f8
Nikolai Arjun Viswanath <viswann@ececomp2.ecn.purdue.edu>
2025-04-04 21:13:01 -0400

    finsihsing queue.c first edition

--- queue.c
+++ queue.c
@@ -1,6 +1,9 @@
 #include "queue.h"
 #include "tile_game.h"
 
+int match_maze(uint8_t tiles1[4][4], uint8 tiles2[4][4]);
+struct 
+
 void enqueue(struct queue *q, struct game_state state) {
 	insert_at_head(&(q->data), serialize(state));
 	return;
@@ -11,6 +14,67 @@ struct game_state dequeue(struct queue *q) {
 	return (deserialize(temp)); 
 }
 
+int match_maze(uint8_t tiles1[4][4], uint8_t tiles2[4][4]){
+	int out = 1;
+	int i;
+	int j;
+	for(i = 0; i<4; i++){
+		for(j = 0; j<4; j++){
+			if(tiles1[i][j] != tiles2[i][j]){
+				out = 0;
+			}
+		}
+	}
+	return out;
+}
+
+
+
+struct game_state copy_state(struct game_state state){
+	struct game_state out = state;
+	return out;
+}
+
+
 int number_of_moves(struct game_state start) { 
+	uint8_t final[4][4];
+	int i;
+	int j;
+	int ct = 1;
+	for(i = 0; i < 4; i++){
+		for (j = 0; j < 4; j++;){
+			final[i][j] = ct;
+			ct ++;
+		}
+	}
+	final[3][3] = 0;
+	struct queue q;
+	enqueue(&q, start);
+	while(q -> data.head != NULL){
+		struct game_state curr;
+		curr = dequeue(&q);
+		if(match_maze(final, curr) == 1){
+			return curr.num_steps;
+		}
+		for(i = 0; i<4; i++){
+			struct game_state next = copy_state(curr);
+			if(i == 0){
+				move_up(next);
+				enqueue(&q, next);
+			}
+			if(i ==1){
+				move_down(next);
+				enqueue(&q, next);
+			}
+			if(i ==2){
+				move_left(next);
+				enqueue(&q, next);
+			}
+			if(i ==3){
+				move_right(next);
+				enqueue(&q, next);
+			}
+		}
+	}
 	return 0; 
 }
